424f8945fd782c0cbdcec7d4bed9ab95899e6444
zoyafathima1 <zoyafathima2004@gmail.com>
2025-10-05 11:44:11 +0530

    Update .gitignore

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '09251ef179b134ae99f35b707310fc2b'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # More config, e.g., database URI, upload folders, etc.
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///yourdatabase.db'
-    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
