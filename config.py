220577dbf3da775d9b370c6c8e1e5dcce9b43f61
zoyafathima1 <zoyafathima2004@gmail.com>
2025-10-05 11:41:11 +0530

    Update .gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///attendance.db'
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'instance', 'student_images')
